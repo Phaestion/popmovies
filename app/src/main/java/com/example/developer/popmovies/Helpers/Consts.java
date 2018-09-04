7439af759b3f16040d8ecd4a7a9cd506abeca191
Steyn Geldenhuys <steyn@truevolve.technology>
2018-09-04 07:51:12 +0000

    Parsing json, created movie_poster_item

--- app/src/main/java/com/example/developer/popmovies/Helpers/Consts.java
+++ app/src/main/java/com/example/developer/popmovies/Helpers/Consts.java
@@ -4,5 +4,10 @@ public class Consts {
 
     //TODO: add API key here
     private static final String apiKey = "";
-    public static final String popMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
+    private static final String baseUrl = "https://api.themoviedb.org/3/movie/";
+
+    public static final String popMoviesUrl = baseUrl + "popular?api_key=" + apiKey;
+    public static final String topMoviesUrl = baseUrl + "top_rated?api_key=" + apiKey;
+
+    public static final String moviePosterUrl = "https://image.tmdb.org/t/p/w185/";
 }
--- app/src/main/java/com/example/developer/popmovies/MainActivity.java
+++ app/src/main/java/com/example/developer/popmovies/MainActivity.java
@@ -1,45 +1,106 @@
 package com.example.developer.popmovies;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
+import com.example.developer.popmovies.Helpers.Consts;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-    ProgressBar loadingProgress;
-    RecyclerView moviePostersRecycler;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ProgressBar loadingProgress;
+    private RecyclerView moviePostersRecycler;
+
+    private String url= Consts.popMoviesUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadingProgress= findViewById(R.id.pb_loading);
-        moviePostersRecycler= findViewById(R.id.rc_movie_posters);
+        loadingProgress = findViewById(R.id.pb_loading);
+        moviePostersRecycler = findViewById(R.id.rc_movie_posters);
+
+        moviePostersRecycler.setHasFixedSize(true);
+        moviePostersRecycler.setLayoutManager(new GridLayoutManager(this, 3));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        loadingProgress.setVisibility(View.VISIBLE);
+        moviePostersRecycler.setVisibility(View.GONE);
+
+        new GetPopularMovies().execute(url);
+    }
+
+
+    public void showMovies(List<MoviePoster> movies) {
 
     }
 
-    private class GetPopularMovies extends AsyncTask<String, Void, List<MoviePoster>>{
-
+    private class GetPopularMovies extends AsyncTask<String, Void, List<MoviePoster>> {
+        private Exception error = null;
 
         @Override
         protected List<MoviePoster> doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+
+                if (response.code() == 200) {
+                    JSONObject job = new JSONObject(response.body().string());
+
+                    JSONArray jarr = job.getJSONArray("results");
+                    ArrayList<MoviePoster> movies = new ArrayList<>();
+
+                    for (int i = 0; i < jarr.length(); i++) {
+                        movies.add(new MoviePoster(jarr.getJSONObject(i)));
+                    }
+
+                    return movies;
+                }
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+                error = e;
+            }
+
             return null;
         }
 
         @Override
         protected void onPostExecute(List<MoviePoster> moviePosters) {
             super.onPostExecute(moviePosters);
+
+            if (moviePosters != null && error == null) {
+                //Successfully got movies
+
+                Log.d(TAG, "Got " + moviePosters.size() + " from themoviedb");
+            }
         }
     }
 }
